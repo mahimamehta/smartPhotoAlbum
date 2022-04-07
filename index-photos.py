25d527689b31f9d5ba7fcdcaea3603c3555f73c9
mahimamehta <mahimamehta.mehta@gmail.com>
2022-04-07 18:13:37 -0400

    adding code pipeline config files

---
+++ index-photos.py
@@ -0,0 +1,62 @@
+import json
+import urllib.parse
+import boto3
+import requests
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def detect_labels(photo, bucket):
+
+    client = boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
+                                    MaxLabels=10)
+
+    print('Detected labels for ' + photo)
+    print()
+    labels = []
+    for label in response['Labels']:
+        labels.append(label['Name'])
+    return labels
+
+
+def storeToES(index, json):
+    host = 'https://search-photos-najvtskrbxogitnfctvxaq2dia.us-east-1.es.amazonaws.com/'
+    path = 'photos/_doc/'
+    url = host + path
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(url, auth=("ES-Demo2", "ES-Demo2"),
+                      data=json, headers=headers)
+    print(r.text)
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    buckets = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(
+        event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    labels = detect_labels(key, buckets)
+    print(key)
+    print(s3.head_object(Bucket=buckets, Key=key))
+    timestamp = event['Records'][0]['eventTime']
+    jsonObj = {
+        "objectKey": key,
+        "bucket": buckets,
+        "createdTimestamp": timestamp,
+        "labels": labels
+    }
+    # print("---------")
+    print(jsonObj)
+    storeToES('photos', json.dumps(jsonObj))
+    try:
+        response = s3.get_object(Bucket=buckets, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, buckets))
+        raise e
---
+++ search-photos.py
@@ -0,0 +1,89 @@
+import json
+import boto3
+import time
+import requests
+import logging
+import os
+
+
+def getSlots(intent_request, context):
+
+    search = intent_request['sessionState']['intent']['slots']['keyword1']['value']['interpretedValue']
+    if intent_request['sessionState']['intent']['slots']['keyword2'] is None:
+        search1 = "None"
+    else:
+        search1 = intent_request['sessionState']['intent']['slots']['keyword2']['value']['interpretedValue']
+    keywords = [search, search1]
+
+    return keywords
+
+
+def getPhotoResults(keywords):
+    endpoint = 'https://search-photos-najvtskrbxogitnfctvxaq2dia.us-east-1.es.amazonaws.com/photos/_search'
+    headers = {'Content-Type': 'application/json'}
+    prepared_q = []
+    for k in keywords:
+        prepared_q.append({"match": {"labels": k}})
+    q = {"query": {"bool": {"should": prepared_q}}}
+    r = requests.post(endpoint, auth=("ES-Demo2", "ES-Demo2"),
+                      headers=headers, data=json.dumps(q))
+    print(r.text)
+    # r = {"took": 7, "timed_out": False, "_shards": {"total": 5, "successful": 5, "skipped": 0, "failed": 0}, "hits": {"total": {"value": 4, "relation": "eq"}, "max_score": 1.0, "hits": [{"_index": "photos", "_type": "photo", "_id": "5uWzhW4B9mNrwci-n6la", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/gettyimages-91495990-170667a.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-220611", "labels": ["Plant", "Tree", "Person", "Human", "Tree Trunk", "Outdoors"]}}, {"_index": "photos", "_type": "photo", "_id": "6OW5hW4B9mNrwci-Yqk8", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/scott-eastwood.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-221229", "labels": ["Person", "Human", "Canine", "Dog", "Pet", "Animal", "Mammal"]}}, {"_index": "photos", "_type": "photo", "_id": "5-W0hW4B9mNrwci-ZqlW", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/pjimage-43-2.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-220702", "labels": ["Tree", "Christmas Tree", "Ornament", "Plant", "Human", "Person"]}}, {"_index": "photos", "_type": "photo", "_id": "5eWzhW4B9mNrwci-V6mF", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/pjimage-43-2.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-220553", "labels": ["Tree", "Ornament", "Christmas Tree", "Plant", "Person", "Human"]}}]}}
+
+    image_array = []
+    for each in r.json()['hits']['hits']:
+        objectKey = each['_source']['objectKey']
+        bucket = each['_source']['bucket']
+        image_url = "https://" + bucket + ".s3.amazonaws.com/" + objectKey
+        image_array.append(image_url)
+        print(each['_source']['labels'])
+    print(image_array)
+    return image_array
+
+
+def dispatch(intent_request, context):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    # logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['sessionState']['intent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'SearchIntent':
+        keywords = getSlots(intent_request, context)
+        print(keywords)
+        getPhotoResults(keywords)
+        response = {'messages': [
+            {'contentType': 'PlainText',
+             'content': 'Here are the search results...'}
+        ], 'sessionState': {
+            'dialogAction': {
+                'type': "Close"
+            },
+            'intent': {
+                'slots': intent_request['sessionState']['intent']['slots'],
+                'name': intent_request['sessionState']['intent']['name'],
+                'state': "Fulfilled"
+            }
+        }}
+    return response
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    # logger.debug('event.bot.name={}'.format(event['bot']['name']))
+    print(event)
+    return dispatch(event, context)
